0dd93dfb7b370970c1036a4c7f6a60f2e8720597
TBatishcheva <tat.batishcheva@gmail.com>
2017-06-24 15:05:11 +0300

    chapter 15 - Implement duplicate item validation at model layer

--- superlists/settings.py
+++ superlists/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -75,12 +74,12 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 print('++++++')
-print(os.path.join(BASE_DIR, '../database/db.sqlite3'))
+print(os.path.join(BASE_DIR, '../superlists/database/db.sqlite3'))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../superlists/database/db.sqlite3'),
     }
 }
 
